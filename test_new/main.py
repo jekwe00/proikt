40dd23dbfb44ba72384d30856a4145a47d471f47
jekwe00 <144591967+jekwe00@users.noreply.github.com>
2023-11-19 12:54:29 +0300

    перадыны значение перменных для шаблона indix.html
    чтобы сделать дз и чота там

--- test_new/main.py
+++ test_new/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html",path_css="/static/css/style.css",path_img="/static/img/men.png")
 
 @app.route('/calc')
 def calc():
